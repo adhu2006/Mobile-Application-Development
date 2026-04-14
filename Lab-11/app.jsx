68426e00d4c15b1894c3f9f1fe889b7b3f1833d1
adhu2006 <sbalamuruganadarshbca24@rvu.edu.in>
2026-04-14 21:57:50 +0530

    Create app.jsx

---
+++ Lab-11/app.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./screens/Home";
+import Student from "./screens/Student";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Student" component={Student} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
